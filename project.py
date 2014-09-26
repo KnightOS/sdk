37d1aa2eab8f6fabeaedb617492d50d95e798df5
Drew DeVault <sir@cmpwn.com>
2014-09-26 16:00:57 -0600

    Remove a few less-than-useful commands

--- project.py
+++ project.py
@@ -1,4 +1,3 @@
-import toml
 from sys import exit, stderr
 from os.path import join, realpath
 from os import listdir, getcwd
@@ -10,7 +9,6 @@ class Project:
 
     def __del__(self):
         pass
-        # self.configfile.write(toml.dumps(self.config))
 
     def open(self, path, mode="r"):
         return open(join(self.root, path), mode=mode) # TODO: This leaks file descriptors
@@ -18,10 +16,9 @@ class Project:
 def findroot():
     path = getcwd()
     while path != "/": # TODO: Confirm this is cross platform
-        if listdir(path).index("knightos.toml") >= 0:
+        if listdir(path).index(".knightos") >= 0:
             return path
         else:
             path = realpath(join(path, ".."))
-    errmsg = "No `knightos.toml' found in any parent of {cwd}.\n"
     stderr.write(errmsg.format(cwd=getcwd()))
     exit(1)
