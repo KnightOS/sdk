8a3c47bbf5bb1e31b91bbeb82918e0e025f73e2a
Max Leiter <maxwell.leiter@gmail.com>
2018-10-19 16:49:52 +0200

    Notify user if invalid package name is passed

--- knightos/commands/install.py
+++ knightos/commands/install.py
@@ -6,7 +6,10 @@ from knightos.workspace import Workspace
 def execute(packages, site_only=False, init=False, local_path=None):
     ws = Workspace()
     for package in packages:
-        if site_only:
+        if len(package.split("/")) != 2:
+            print("Invalid package name {0}. Format should be `repository/package`.".format(package))
+            break;
+        elif site_only:
             ws.install_package(package, local_path=local_path)
         else:
             ws.require_package(package, local_path=local_path)
